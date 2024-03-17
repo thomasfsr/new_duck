50271f1810627787d1cba4d4fda12796c980aa11
Thomás Freire <thomas.fsr@gmail.com>
2024-03-17 18:48:25 -0300

    infering the schema of the data

---
+++ app/infer.py
@@ -0,0 +1,15 @@
+import os
+import duckdb
+import pandas as pd
+from pandera import infer_schema
+
+def infer(df:pd.DataFrame, directory_path:str= 'infered_schema'):
+    os.makedirs(directory_path, exist_ok=True)
+    schema = infer_schema(df)
+    file_path = os.path.join(directory_path, "schema_inferred.py")
+    with open(file_path, "w", encoding="utf-8") as arquivo:
+        arquivo.write(schema.to_script())
+
+if __name__ == "__main__":
+    df = duckdb.read_parquet('data/daily_sales_retail_0.parquet').fetchdf()
+    infer(df)
