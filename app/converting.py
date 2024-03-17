255e0a43ce7ab740fc07882fdf76ac5e210a1809
Thomás Freire <thomas.fsr@gmail.com>
2024-03-17 18:36:03 -0300

    documentation

---
+++ app/converting.py
@@ -0,0 +1,16 @@
+import os
+
+import duckdb
+from dotenv import load_dotenv
+
+load_dotenv()
+
+path = os.environ.get("path_csvs")
+
+os.makedirs("data", exist_ok=True)
+for file in os.listdir(path):
+    filename = file.split(".", 1)[0]
+    parquetname = filename + ".parquet"
+    duckdb.read_csv(os.path.join(path, file)).to_parquet(
+        os.path.join("data", parquetname)
+    )
---
+++ app/extract.py
@@ -0,0 +1,43 @@
+import io
+
+from dotenv import load_dotenv
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+
+import os
+
+import duckdb
+load_dotenv()
+
+def create_data_dir():
+    os.makedirs("data", exist_ok=True)
+
+
+def download_from_gdrive():
+    data_folder = "data"
+    folder_id = os.environ.get("folder_id")
+    credentials = service_account.Credentials.from_service_account_file(
+        "credentials/api_key.json"
+    )
+
+    drive_service = build("drive", "v3", credentials=credentials)
+
+    files = (
+        drive_service.files()
+        .list(q=f"'{folder_id}' in parents and mimeType='application/octet-stream'")
+        .execute()
+    )
+
+    for file in files.get("files", []):
+        file_id = file["id"]
+        file_name = file["name"]
+        request = drive_service.files().get_media(fileId=file_id)
+        fh = io.BytesIO()
+        downloader = io.BytesIO(request.execute())
+        with open(os.path.join(data_folder, file_name), "wb") as f:
+            f.write(downloader.read())
+
+
+data_folder = "data"
+table = duckdb.read_parquet(f"{data_folder}/*.parquet").fetchdf()
+print(table)
