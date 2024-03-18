d81f127ba551cc001fa083b1ee6f68cf560d2833
Thomás Freire <thomas.fsr@gmail.com>
2024-03-17 22:47:09 -0300

    pipeline

--- app/pipeline.py
+++ app/pipeline.py
@@ -1,14 +1,15 @@
-from app import extract, transform
+import extract, transform
 import os
 
-def pipeline(output:str='database',
+def pipeline(output_dir:str='database',
              table_name:str='sales_table.parquet'
              ):
+    os.makedirs(output_dir, exist_ok=True )
     extract.create_data_dir()
     extract.download_from_gdrive()
     passed = transform.validation('data')
     df = transform.concating(passed)
-    df.to_parquet(os.path.join(output,table_name))
+    df.to_parquet(os.path.join(output_dir,table_name))
 
 if __name__ == "__main__":
     pipeline()
